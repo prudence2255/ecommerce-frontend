304471e3da829b7c332742caceed832169f5fbd6
prudence2255 <enthusiastgh@gmail.com>
2020-11-20 01:06:28 +0000

    css

--- components/ad/images.js
+++ components/ad/images.js
@@ -16,18 +16,8 @@ const Images = ({ad}) => {
         <style jsx>
             {`
             .image-gallery-slide img {
-    width: 100%;
-    height: 400px;
-    max-height: 80vh;
-    object-fit: cover;
-    overflow: hidden;
-    object-position: center center;
-}
-
-    .fullscreen .image-gallery-slide img {
-        height: 400px;
-     max-height: 100vh;
-}
+            height: 400px;
+            }
             `}
         </style>
        
