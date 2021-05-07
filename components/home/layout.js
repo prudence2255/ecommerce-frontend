ee781c4ddb0129aef81976697a1ddc31e1628c4d
Alidu Bakpata Latif <aliduarahim@gmail.com>
2021-05-07 12:57:18 +0000

    error boundary added

--- components/home/layout.js
+++ components/home/layout.js
@@ -2,12 +2,23 @@ import Head from 'next/head';
 import React, {useState} from 'react';
 import Header from './header';
 import Footer from './footer';
+import {ErrorBoundary} from 'react-error-boundary';
 
+
+function ErrorFallback({error}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{color: 'red'}}>{error.message}</pre>
+    </div>
+  )
+}
 
 
 export default function Layout({children}){
     return(
         <>
+     <ErrorBoundary FallbackComponent={ErrorFallback}>
        <Head>
          <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
         </Head>
@@ -20,6 +31,7 @@ export default function Layout({children}){
       </div>
       <Footer />
      </div>
-        </>
+     </ErrorBoundary>
+     </>
     )
 }
